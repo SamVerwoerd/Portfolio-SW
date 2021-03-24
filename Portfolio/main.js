bd7e26adce7423a494d09d8c6e9433524d42dc80
SamVerwoerd <70705295+SamVerwoerd@users.noreply.github.com>
2021-03-24 22:19:35 +0100

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('.theme');
+const elements = document.querySelectorAll('body, h1, .themed, li, .card-body, .card-img-top, .card, .footer, button');
+
+let darkMode = false
+
+const colorHandler = function(){
+    for(let i = 0;i < elements.length ;i++){
+        elements[i].classList.toggle('dark-theme')
+    }
+    if(button.textContent == "Light Mode"){
+        button.textContent = "Dark Mode";
+    }
+    else{
+        button.textContent = "Light Mode";
+    }
+}
+
+button.addEventListener('click', colorHandler);
